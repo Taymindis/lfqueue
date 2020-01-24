8f403d1693b821f8b03f561a0708d0a8edc46b01
dbjdbj <dbj@dbj.org>
2020-01-24 18:17:17 +0100

    tuning the test

--- lfqueue_vs201x/cpp/test.cpp
+++ lfqueue_vs201x/cpp/test.cpp
@@ -5,10 +5,9 @@ namespace lf_queue {
 
 	namespace test {
 
-		//-----------------------------------------------------------------------------------------------
-//		struct timeval  tv1, tv2;
-#define total_put 50000
-#define total_running_loop 50
+		/// ------------------------------------------------------------------------------------------ 
+		constexpr auto total_put = 50000U;
+		constexpr auto total_running_loop = 50U;
 		constexpr int nthreads = 4;
 		int one_thread = 1;
 		int nthreads_exited = 0;
@@ -36,18 +35,20 @@ namespace lf_queue {
 
 		void one_enq_and_multi_deq(threads_arr_type&);
 		void one_deq_and_multi_enq(threads_arr_type&);
-		void multi_enq_deq(std::thread* threads);
-		void* worker_sc(void*);
-		void* worker_s(void*);
-		void* worker_c(void*);
+		void multi_enq_deq(threads_arr_type&);
+		
+		void* worker_sender_consumer(void*);
+		void* worker_sender(void*);
+		void* worker_consumer(void*);
 		void* worker_single_c(void*);
 
 		/**Testing must**/
 		void one_enq_and_multi_deq_must(threads_arr_type&);
 		void one_deq_must_and_multi_enq(threads_arr_type&);
-		void multi_enq_deq_must(std::thread* threads);
+		void multi_enq_deq_must(threads_arr_type&);
+
 		void* worker_sc_must(void*);
-		void* worker_s_must(void*);
+		// void* worker_s_must(void*);
 		void* worker_c_must(void*);
 		void* worker_single_c_must(void*);
 
@@ -105,111 +106,83 @@ namespace lf_queue {
 				message_type payload_received = (message_type)lfqueue_deq_must(lfq_ptr_.get());
 				if (payload_received)
 				  printf("\n%s", payload_received);
+
+				if (payload_received) printf("\n" __FUNCSIG__  " -- received -- %s\n", payload_received);
 			}
 			return 0;
 		}
 		/// ---------------------------------------------------------------------------------------------
 
-		void* worker_c(void* arg) {
-			int multiplier_{ 1 };
-			if (arg) {
-				multiplier_ = int(*((int*)arg));
-			}
-			int total_loop = total_put * multiplier_;
+		void* worker_consumer(void* arg) {
+
+			int total_loop = total_put ;
+			printf("\n");
 			while (--total_loop) {
 				/*Dequeue*/
 				message_type data_{};
 				while (( data_ = (message_type)lfqueue_deq(lfq_ptr_.get())) == nullptr) {
 					lfqueue_sleep(1);
 				}
-				//	printf("%s\n", data_);
+				if(data_) printf( "\r worker_consumer <-- received --| %32s [%6d]",  data_, total_loop);
 			}
+			printf("\n");
 			return 0;
 		}
 
 		/// ---------------------------------------------------------------------------------------------
 		void* worker_single_c(void* arg) {
-			int multiplier_{ 1 };
-			if (arg) {
-				multiplier_ = int(*((int*)arg));
-			}
-			int total_loop = total_put * multiplier_;
+			int total_loop = total_put ;
 			while (--total_loop) {
 				/*Dequeue*/
 				message_type data_{};
 				while ((data_ = (message_type)lfqueue_single_deq(lfq_ptr_.get())) == nullptr) {
 					lfqueue_sleep(1);
 				}
-				//	printf("%s\n", data_);
+				if (data_) printf("\nworker_single_c-- received -- %s\n", data_);
 			}
 			return 0;
 		}
 
 		/// ---------------------------------------------------------------------------------------------
 		/** Worker Keep Sending at the same time, do not try instensively **/
-		void* worker_s(void* arg)
+		void* worker_sender(void* arg)
 		{
-			int multiplier_{ 1 };
+			int total_loop = total_put ;
 
-			if (arg) {
-				multiplier_ = int(*((int*)arg));
-			}
+			static message_type payload = " worker_sender message "  ;
 
-			static message_type payload = "PAYLOAD";
-			int walker_ = 0;
-			int total_loop = total_put * multiplier_;
-			while (walker_++ < total_loop) {
+			while (--total_loop) {
 
 				/*Enqueue*/
 				while (lfqueue_enq(lfq_ptr_.get(), &payload))
 				{
-					// printf("ENQ FULL?\n");
+					printf( " worker_sender() -- ENQ FULL?\n");
 				}
+
+				lfqueue_sleep(1); // sleep one millisecond
 			}
-			// __sync_add_and_fetch(&nthreads_exited, 1);
 			return 0;
 		}
 
+		/// ---------------------------------------------------------------------------------------------
 		/** Worker Send And Consume at the same time **/
-		void* worker_sc(void* arg)
+		void* worker_sender_consumer(void* arg)
 		{
-			int i = 0;
-			int* int_data;
-			while (i < total_put) {
-				int_data = (int*)malloc(sizeof(int));
-				assert(int_data != NULL);
-				*int_data = i++;
+			int total_loop = total_put ;
+			static message_type payload = "worker_sender_consumer message";
+			while (--total_loop) {
 				/*Enqueue*/
-				while (lfqueue_enq(lfq_ptr_, int_data)) {
+				while (lfqueue_enq(lfq_ptr_.get(), (void*)payload)) {
 					printf("ENQ FULL?\n");
 				}
-
 				/*Dequeue*/
-				while ((int_data = lfqueue_deq(lfq_ptr_)) == NULL) {
+				while ((payload = (message_type)lfqueue_deq(lfq_ptr_.get())) == nullptr) {
 					lfqueue_sleep(1);
 				}
-				// printf("%d\n", *int_data);
-				free(int_data);
+				if (payload) printf("\rworker_sender_consumer -- received -- %30s [%6d]", payload, total_loop);
 			}
-			__sync_add_and_fetch(&nthreads_exited, 1);
 			return 0;
 		}
-
-#if 0
-#define join_threads \
-for (i = 0; i < nthreads; i++) {\
-pthread_join(threads[i], NULL); \
-}
-
-#define detach_thread_and_loop \
-for (i = 0; i < nthreads; i++)\
-pthread_detach(threads[i]);\
-while ( nthreads_exited < nthreads ) \
-	lfqueue_sleep(10);\
-if(lfqueue_size(lfq_ptr_ ) != 0){\
-lfqueue_sleep(10);\
-}
-#endif // 0
 
 		/// --------------------------------------------------------------------------------------------
 		/// functions given to running_test()
@@ -219,7 +192,7 @@ lfqueue_sleep(10);\
 			printf("-----------%s---------------\n", "multi_enq_deq");
 
 			for (auto& thr_ : threads) {
-				thr_ = std::thread(worker_sc);
+				thr_ = std::thread(worker_sender_consumer, nullptr);
 			}
 
 			for (auto& thr_ : threads) {
@@ -231,32 +204,33 @@ lfqueue_sleep(10);\
 			printf("-----------%s---------------\n", "one_deq_and_multi_enq");
 
 			for (auto& thr_ : threads) {
-				thr_ = std::thread(worker_s);
+				thr_ = std::thread(worker_sender,nullptr);
 			}
 
-			worker_single_c(threads);
+			worker_single_c(nullptr);
 
 			for (auto& thr_ : threads) {
 				thr_.join();
 			}
 		}
-
+		///-------------------------------------------------------------------------------------------
 		void one_enq_and_multi_deq(threads_arr_type& threads) {
 			printf("-----------%s---------------\n", "one_enq_and_multi_deq");
 
 			for (auto& thr_ : threads) {
-				thr_ = std::thread(worker_c);
+				thr_ = std::thread(worker_consumer,nullptr);
 			}
 
-			worker_s(threads);
+			worker_sender(nullptr);
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wimplicit-function-declaration"
-			for (auto& thr_ : threads) {
+			for (auto& thr_ : threads) 
+			{
+				while (false == thr_.joinable())
+				{
+					lfqueue_sleep(1);
+				}
 				thr_.join();
 			}
-#pragma GCC diagnostic pop
-
 		}
 
 
@@ -264,7 +238,7 @@ lfqueue_sleep(10);\
 			printf("-----------%s---------------\n", "one_deq_must_and_multi_enq");
 
 			for (auto& thr_ : threads) {
-				thr_ = std::thread(worker_s);
+				thr_ = std::thread(worker_sender, nullptr);
 			}
 
 			worker_single_c_must(nullptr);
@@ -278,24 +252,20 @@ lfqueue_sleep(10);\
 			printf("-----------%s---------------\n", "one_enq_and_multi_deq_must");
 
 			for (auto& thr_ : threads) {
-				thr_ = std::thread(worker_c_must);
+				thr_ = std::thread(worker_c_must, nullptr);
 			}
 
-			worker_s(nullptr);
+			worker_sender(nullptr);
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wimplicit-function-declaration"
 			for (auto& thr_ : threads) {
 				thr_.join();
 			}
-#pragma GCC diagnostic pop
-
 		}
 
 		void multi_enq_deq_must(threads_arr_type& threads) {
 			printf("-----------%s---------------\n", "multi_enq_deq_must");
 			for (auto& thr_ : threads) {
-				thr_ = std::thread(worker_sc_must);
+				thr_ = std::thread(worker_sc_must, nullptr );
 			}
 			for (auto& thr_ : threads) {
 				thr_.join();
@@ -305,11 +275,9 @@ lfqueue_sleep(10);\
 		/// --------------------------------------------------------------------------------------------
 
 		void running_test(test_function testfn) {
-			int n;
 
-			for (n = 0; n < total_running_loop; n++) {
-				printf("Running loop %d or %d ", n, total_running_loop);
-				nthreads_exited = 0;
+			for (int n = 0; n < total_running_loop; n++) {
+				printf("Running loop %4d of %4d ", n, total_running_loop);
 
 				/* Spawn N threads. */
 				threads_arr_type threads_arr_{ {} };
@@ -320,10 +288,10 @@ lfqueue_sleep(10);\
 				timer timer_{};
 				testfn(threads_arr_);
 
-				printf("Total time: %f seconds\n", as_buffer(timer_, timer::kind::second).data());
+				printf("Total time: %s seconds\n", as_buffer(timer_, timer::kind::second).data());
 
 				lfqueue_sleep(10);
-				assert(0 == lfqueue_size(lfq_ptr_.get()) && "Error, all queue should be consumed but is not");
+				assert(0 == lfqueue_size(lfq_ptr_.get()) && "Error, all queues should be consumed but they are not");
 			}
 		}
 
@@ -344,14 +312,13 @@ int main(const int, const char**)
 	}
 
 	running_test(one_enq_and_multi_deq);
+	/*
 	running_test(one_enq_and_multi_deq_must);
-
 	running_test(one_deq_and_multi_enq);
 	running_test(one_deq_must_and_multi_enq);
-
 	running_test(multi_enq_deq);
 	running_test(multi_enq_deq_must);
-
+	*/
 
 	printf("All Tests have passed!\n");
 
